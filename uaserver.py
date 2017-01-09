fabaee3dab815b03123f08ad27628e80fc68506d
prodaromero <elrodas95@gmail.com>
2017-01-09 18:09:32 +0100

    abriendo comunicacion

--- uaserver.py
+++ uaserver.py
@@ -65,6 +65,8 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     Echo server class
     """
     METHOD_CLASS = ['INVITE', 'ACK', 'BYE']
+    IpClient = []
+    PortClient = []
 
     def handle(self):
         while 1:
@@ -72,18 +74,23 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             line = self.rfile.read()
             if not line:
                 break
-            Package = line.decode('utf-8')
-            print("El cliente nos manda " + Package)
-            Package = Package.split(' ')
+            M_Recieve = line.decode('utf-8')
+            print("El cliente nos manda:\r\n" + M_Recieve)
+            Line = M_Recieve.split('\r\n')
+            Package = Line[0].split(' ')
             METHOD = Package[0]
             print(METHOD)
             if METHOD == 'INVITE':
-                Message = (" SIP/2.0 100 Trying\r\n" + "SIP/2.0 180 Ring\r\n" +
+
+                IpClient = Line[4].split(' ')[1]
+                Message = ("SIP/2.0 100 Trying\r\n" + "SIP/2.0 180 Ring\r\n" +
                            "SIP/2.0 200 OK\r\n" + 
                            "Content-Type: application/sdp\r\n\r\n" + "v=0\r\n"
                            + username + " " + ipServer + "\r\n" + 
                            "s=misesion\r\n" + "t=0\r\n" + "m=" + 
                            str(elementos[5].tag) + " " + portRtp + " RTP")
+                self.wfile.write(bytes(Message, 'utf-8') + b'\r\n')
+                print("Enviando:\r\n" + Message)
             elif METHOD == 'ACK':
                 # aEjecutar es un string con lo que se ha de ejecutar en la shell
                 aEjecutar = "./mp32rtp -i " + ipServer + " -p " + portRtp
@@ -97,12 +104,15 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             else:
                 Message = "SIP/2.0 400 Bad Request\r\n\r\n"
 
+            print("Enviando:\r\n" + Message)
+
 
 if __name__ == "__main__":
 
     #Creamos un servidor y escuchamos
     print("Listening...")
-    
+    print(ipServer)
+    print(portServer)
     serv = socketserver.UDPServer((ipServer, int(portServer)), EchoHandler)
 
     print("Lanzando servidor UDP de eco...")
