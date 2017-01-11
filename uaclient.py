aaef01dd7061f323d513c5cfd959352879e6a09a
Pablo Roda Romero <proda@alpha13.aulas.gsyc.urjc.es>
2017-01-11 17:49:36 +0100

    pep8

--- uaclient.py
+++ uaclient.py
@@ -10,6 +10,7 @@ from lxml import etree
 import time
 import os
 
+
 try:
     CONFIG = str(sys.argv[1])
     METODO = sys.argv[2]
@@ -22,15 +23,28 @@ try:
 except:
     sys.exit("Usage: The file is not a .xml or does not exist")
 
-#Creamos nuestra estructura de datos ElementTree
+
+def CreateLog(Log, Time, Message):
+
+    """
+    CREAMOS ARCHIVO LOG
+    """
+
+    FichLog = open(Log, 'a')
+    FichLog.write(Time)
+    FichLog.write(Message)
+    FichLog.close
+
+# Creamos nuestra estructura de datos ElementTree
 doc = etree.parse(CONFIG)
 
-#Obtenemos nuestros elementos que forman ElementTree
+# Obtenemos nuestros elementos que forman ElementTree
 elementos = doc.getroot()
 
-#Obtenemos nuestras variables
+# Obtenemos nuestras variables
 account = elementos[0].attrib
 username = account.get("username")
+print(username)
 passwd = account.get("passwd")
 
 uaserver = elementos[1].attrib
@@ -56,92 +70,158 @@ print("-----------\r\n")
 hora = time.time()
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((ipProxy, int(portProxy)))
 
 
-#try:
-Metodos = ['REGISTER', 'INVITE', 'BYE']
+try:
+    Metodos = ['REGISTER', 'INVITE', 'BYE']
 
-Message = METODO + " sip:"
-if METODO == "REGISTER":
-    Message = (Message + username +  ":" + str(portServer)
-               + " SIP/2.0\r\n" + "Expires: " + str(OPCION) + "\r\n")
-    print("Enviando: " + Message)
-    print("-----------\r\n")
-    #Enviamos el mensaje Register
-    my_socket.send(bytes(Message, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
+    Cabecera = METODO + " sip:"
+    if METODO == "REGISTER":
 
-    #Esperamos a recibir la Autorizacion
-    M_Recieve = data.decode('utf-8')
-    print("Recibido -- " + M_Recieve + "\r\n")
-    print("-----------\r\n")
-    Autorizacion = M_Recieve.split("\r\n")[1]
-    nonce = Autorizacion.split('=')[1]
-    Message = Message + Autorizacion
+        open(pathLog, 'w')
+        Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        Message = " Starting..."
+        CreateLog(pathLog, Time, Message + "\r\n")
+        Message = (Cabecera + username + ":" + str(portServer) +
+                   " SIP/2.0\r\n" + "Expires: " + str(OPCION) + "\r\n")
 
-    #Enviamos el nuevo mensaje
-    my_socket.send(bytes(Message, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print("Enviando:\r\n" + Message + "\r\n")
-    print("-----------\r\n")
-
-    M_Recieve = data.decode('utf-8')
-    print("Recibido -- " + M_Recieve + "\r\n")
-    print("-----------\r\n")
-elif METODO == "INVITE":
-    Message = (Message + OPCION + " SIP/2.0\r\n" 
-               + "Content-Type: application/sdp\r\n\r\n" + "v=0\r\n" + "o="
-               + username + " " + ipServer + "\r\n" + "s=misesion\r\n"
-               + "t=0\r\n" + "m=" + str(elementos[5].tag) + " "
-               + portRtp + " RTP")
-    print("Enviando:\r\n" + Message + "\r\n")
-    print("-----------\r\n")
-    my_socket.send(bytes(Message, 'utf-8') + b'\r\n')
-
-    #Datos recividos del Servidor
-    data = my_socket.recv(1024)
-    respuesta = data.decode('utf-8')
-    print("Recibiendo del Servidor:\r\n" + respuesta)
-    print("-----------\r\n")
-    Lines = respuesta.split('\r\n')
-    Trying = Lines[0].split(' ')[1]
-    Rining = Lines[1].split(' ')[1]
-    OK = Lines[2].split(' ')[1]
-    Client = Lines[6].split(' ')[0]
-    UserClient = Client.split('=')[1]
-
-    if Trying == '100' and Rining == '180' and OK == '200':
-        Message = ('ACK sip:' + UserClient + " SIP/2.0")
+        print("Enviando: " + Message)
+        print("-----------\r\n")
+        # Enviamos el mensaje Register
         my_socket.send(bytes(Message, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+
+        # LOG
+        Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        MessageLog = (" Sent to " + ipProxy + ":" + portProxy + ": " +
+                      Cabecera + username + " [...]")
+        CreateLog(pathLog, Time, MessageLog + "\r\n")
+
+        # Esperamos a recibir la Autorizacion
+        M_Recieve = data.decode('utf-8')
+        print("Recibido -- " + M_Recieve + "\r\n")
+        print("-----------\r\n")
+        Autorizacion = M_Recieve.split("\r\n")[1]
+        nonce = Autorizacion.split('=')[1]
+
+        # LOG
+        Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        MessageLog = (" Recieved from: " + ipProxy + ":" + portProxy + ": " +
+                      Autorizacion + " [...]")
+        CreateLog(pathLog, Time, MessageLog + "\r\n")
+
+        Message = Message + Autorizacion
+
+        # Enviamos el nuevo mensaje
+        my_socket.send(bytes(Message, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
         print("Enviando:\r\n" + Message + "\r\n")
         print("-----------\r\n")
 
-elif METODO == "BYE":
-    Message = (Message + OPCION + " SIP/2.0")
-    print("Enviando:\r\n" + Message + "\r\n")
-    print("-----------\r\n")
-    my_socket.send(bytes(Message, 'utf-8') + b'\r\n')
+        # LOG
+        Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        MessageLog = (" Sent to " + ipProxy + ":" + portProxy + ": " +
+                      Autorizacion + " [...]")
+        CreateLog(pathLog, Time, MessageLog + "\r\n")
 
-    data = my_socket.recv(1024)
-    respuesta = data.decode('utf-8')
-    print("Recibiendo del Servidor:\r\n" + respuesta)
-    print("-----------\r\n")
-#except:
-#        sys.exit("Usage: Method must be REGISTER, INVITE or BYE")
+        M_Recieve = data.decode('utf-8')
+        print("Recibido -- " + M_Recieve + "\r\n")
+        print("-----------\r\n")
+
+        # LOG
+        Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        MessageLog = (" Recieved from: " + ipProxy + ":" + portProxy + ": " +
+                      M_Recieve)
+        CreateLog(pathLog, Time, MessageLog + "\r\n")
+
+    elif METODO == "INVITE":
+
+        open(pathLog, 'a')
+
+        Message = (Cabecera + OPCION + " SIP/2.0\r\n" +
+                   "Content-Type: application/sdp\r\n\r\n" + "v=0\r\n" +
+                   "o=" + username + " " + ipServer + "\r\n" +
+                   "s=misesion\r\n" + "t=0\r\n" + "m=" +
+                   str(elementos[5].tag) +
+                   " " + portRtp + " RTP")
+        print("Enviando:\r\n" + Message + "\r\n")
+        print("-----------\r\n")
+        my_socket.send(bytes(Message, 'utf-8') + b'\r\n')
+
+        # LOG
+        Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        MessageLog = (" Sent to " + ipProxy + ":" + portProxy + ": " + METODO +
+                      " " + OPCION + " [...]")
+        CreateLog(pathLog, Time, MessageLog + "\r\n")
+
+        # Datos recividos del Servidor
+        data = my_socket.recv(1024)
+        respuesta = data.decode('utf-8')
+        print("Recibiendo del Servidor:\r\n" + respuesta)
+        print("-----------\r\n")
+        Lines = respuesta.split('\r\n')
+        Trying = Lines[0].split(' ')[1]
+        Rining = Lines[1].split(' ')[1]
+        OK = Lines[2].split(' ')[1]
+        Client = Lines[6].split(' ')[0]
+        UserClient = Client.split('=')[1]
+
+        # LOG
+        Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        MessageLog = (" Recieved from: " + ipProxy + ":" + portProxy + ": " +
+                      Lines[0] + " [...]")
+        CreateLog(pathLog, Time, MessageLog + "\r\n")
+
+        if Trying == '100' and Rining == '180' and OK == '200':
+            Message = ('ACK sip:' + UserClient + " SIP/2.0")
+            my_socket.send(bytes(Message, 'utf-8') + b'\r\n')
+            print("Enviando:\r\n" + Message + "\r\n")
+            print("-----------\r\n")
+
+            # LOG
+            Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+            MessageLog = (" Sent to: " + ipProxy + ":" + portProxy +
+                          ": ACK [...]")
+            CreateLog(pathLog, Time, MessageLog + "\r\n")
+        else:
+            # LOG
+            Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+            MessageLog = (" Recieve from: " + ipProxy + ":" + portProxy +
+                          ": 404 User Not Foun")
+            CreateLog(pathLog, Time, MessageLog + "\r\n")
+
+    elif METODO == "BYE":
+        Message = (Cabecera + OPCION + " SIP/2.0")
+        print("Enviando:\r\n" + Message + "\r\n")
+        print("-----------\r\n")
+        my_socket.send(bytes(Message, 'utf-8') + b'\r\n')
+
+        # LOG
+        Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        MessageLog = (" Sent to: " + ipProxy + ":" + portProxy + ": BYE [...]")
+        CreateLog(pathLog, Time, MessageLog + "\r\n")
+
+        data = my_socket.recv(1024)
+        respuesta = data.decode('utf-8')
+
+        # LOG
+        Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        MessageLog = (" Recieve from: " + ipProxy + ":" + portProxy +
+                      ": BYE [...]")
+        CreateLog(pathLog, Time, MessageLog + "\r\n")
+
+        print("Recibiendo del Servidor:\r\n" + respuesta)
+        print("-----------\r\n")
+
+        # LOG
+        Time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        MessageLog = (" Finishing.")
+        CreateLog(pathLog, Time, MessageLog + "\r\n")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+except:
+        sys.exit("Error: No server listening")
